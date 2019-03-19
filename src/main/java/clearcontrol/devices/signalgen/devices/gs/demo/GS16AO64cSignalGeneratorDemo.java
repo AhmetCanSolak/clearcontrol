bffbc8379b4f15477e27829e9d1fb740d51d6e15
AhmetCanSolak <asolak14@ku.edu.tr>
2019-03-19 15:01:47 -0700

    second demo case added for GS wrapper

--- src/main/java/clearcontrol/devices/signalgen/devices/gs/demo/GS16AO64cSignalGeneratorDemo.java
+++ src/main/java/clearcontrol/devices/signalgen/devices/gs/demo/GS16AO64cSignalGeneratorDemo.java
@@ -15,12 +15,32 @@ import static org.junit.Assert.assertTrue;
 public class GS16AO64cSignalGeneratorDemo
 {
     @Test
-    public void demo1()
+    public void demo_Simple8ChannelSinus()
     {
         final GS16AO64cSignalGenerator lGS16AO64cSignalGenerator = new GS16AO64cSignalGenerator();
         assertTrue(lGS16AO64cSignalGenerator.open());
 
         final ScoreInterface lScore = buildScore();
+        final ScoreVisualizerJFrame lVisualize = ScoreVisualizerJFrame.visualize("demo1",lScore);
+
+        for (int i = 0; i < 1000000000 && lVisualize.isVisible(); i++)
+        {
+            lGS16AO64cSignalGenerator.playScore(lScore);
+            System.out.println(i);
+        }
+
+        lVisualize.dispose();
+
+        assertTrue(lGS16AO64cSignalGenerator.close());
+    }
+
+    @Test
+    public void demo_16_48ChannelsVariousSinuses()
+    {
+        final GS16AO64cSignalGenerator lGS16AO64cSignalGenerator = new GS16AO64cSignalGenerator();
+        assertTrue(lGS16AO64cSignalGenerator.open());
+
+        final ScoreInterface lScore = buildScoreConstantDurationVariousSinuses();
         final ScoreVisualizerJFrame lVisualize = ScoreVisualizerJFrame.visualize("demo1",lScore);
 
         for (int i = 0; i < 1000000000 && lVisualize.isVisible(); i++)
@@ -47,6 +67,29 @@ public class GS16AO64cSignalGeneratorDemo
 
         for (int i = 0; i < 8; i++)
             lMeasure.setStave(i, lSinusStave1);
+
+        lMeasure.setDuration(1, TimeUnit.SECONDS);
+        lScore.addMeasureMultipleTimes(lMeasure, 10);
+
+        return lScore;
+    }
+
+    private ScoreInterface buildScoreConstantDurationVariousSinuses()
+    {
+        final Score lScore = new Score("Test Score");
+        final Measure lMeasure = new Measure("Test Measure", 32);
+
+        for (int i = 16; i < 24; i++)
+            lMeasure.setStave(i, new SinusStave("sinus1", 1f, 0f, 1f));
+
+        for (int i = 24; i < 32; i++)
+            lMeasure.setStave(i, new SinusStave("sinus2", 1f, 0.5f, 1f));
+
+        for (int i = 32; i < 40; i++)
+            lMeasure.setStave(i, new SinusStave("sinus3", 1f, 0f, 0.5f));
+
+        for (int i = 40; i < 48; i++)
+            lMeasure.setStave(i, new SinusStave("sinus4", 0.5f, 0f, 1f));
 
         lMeasure.setDuration(1, TimeUnit.SECONDS);
         lScore.addMeasureMultipleTimes(lMeasure, 10);
